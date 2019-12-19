d62a9953b473692a69364b10fbf340061bf722f3
Arx1971 <adnan.rahin13@gmail.com>
2019-12-19 06:56:34 -0500

    update

--- activation_value/__init__.py
+++ activation_value/__init__.py
@@ -1,16 +1,13 @@
-import math
+import numpy as np
 
 
 def sigmoid_function(z):
-    return float(1 / (1 + math.exp(-z)))
+    return float(1 / (1 + np.exp(-z)))
 
 
 def tanh_function(z):
-    return float((math.exp(z) - math.exp(-z)) / (math.exp(-z) + math.exp(z)))
+    return float((np.exp(z) - np.exp(-z)) / (np.exp(-z) + np.exp(z)))
 
 
 def relu(z):
     return max(0.0, z)
-
-
-print(tanh_function(10))
