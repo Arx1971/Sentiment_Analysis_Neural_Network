a6e7fbf439d434bee23d716a206cea8657d9f820
Arx1971 <adnan.rahin13@gmail.com>
2019-12-21 14:01:32 -0500

    File Reader Added

--- logisticregression/filereader.py
+++ logisticregression/filereader.py
@@ -9,3 +9,15 @@ def read_all_file_name(filepath):
     return files
 
 
+def main():
+    file_path_train = "/home/adnan/source-code/PycharmProjects/Sentiment_Classification_Logistic_Regeression/aclImdb" \
+                      "/train"
+    file_path_test = "/home/adnan/source-code/PycharmProjects/Sentiment_Classification_Logistic_Regeression/aclImdb" \
+                     "/test"
+    train_pos_file_names = read_all_file_name(file_path_train + "/pos")
+
+    print(train_pos_file_names)
+
+
+if __name__ == '__main__':
+    main()
