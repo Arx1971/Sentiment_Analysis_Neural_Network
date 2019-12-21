600d6f63f4de2ebd826692bfdfbfc4be07f45ab6
Arx1971 <adnan.rahin13@gmail.com>
2019-12-21 11:56:49 -0500

    activation value

---
+++ logisticregression/activationvalues.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def sigmoid_function(z):
+    return float(1 / (1 + np.exp(-z)))
+
+
+def tanh_function(z):
+    return float((np.exp(z) - np.exp(-z)) / (np.exp(-z) + np.exp(z)))
+
+
+def ReLU(z):
+    return max(0.0, z)
