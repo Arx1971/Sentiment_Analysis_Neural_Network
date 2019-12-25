1093c83f96a46b9062942b108e0e9cb605728e4f
Arx1971 <adnan.rahin13@gmail.com>
2019-12-25 07:24:18 -0500

    update logistic regression

--- logisticregression/ngramsmodel.py
+++ logisticregression/ngramsmodel.py
@@ -4,6 +4,8 @@ from nltk.tokenize import word_tokenize
 import string
 
 
+# nltk.download('stopwords') download all the stopwords
+
 def dictionary_frequency_viewer(dictionary):
     for values, key in dictionary.items():
         print(values, key)
@@ -11,20 +13,27 @@ def dictionary_frequency_viewer(dictionary):
 
 def bag_of_word_model(reviews):
     dictionary = dict()
-    # nltk.download('stopwords') download all the stopwords
+    idf_dictionary = dict()
+    frequency = set()
     stop_words = set(stopwords.words('english'))
     with open(reviews, 'r') as lines:
         for line in lines:
             words = line.split()
             for word in words:
+                frequency.add(word)
                 if word not in stop_words:
                     if word in dictionary:
                         dictionary[word] += 1
                     else:
                         dictionary[word] = 1
-    return dictionary
+            for word in frequency:
+                if word in idf_dictionary:
+                    idf_dictionary[word] += 1
+                else:
+                    idf_dictionary[word] = 1
+    return dictionary, idf_dictionary
 
 
-positive_vocabulary = bag_of_word_model('positive_reviews_train.txt')
-negative_vocabulary = bag_of_word_model('negative_reviews_train.txt')
+positive_vocabulary = bag_of_word_model('positive_reviews_train.txt')[0]
+negative_vocabulary = bag_of_word_model('negative_reviews_train.txt')[0]
 dictionary_frequency_viewer(negative_vocabulary)
